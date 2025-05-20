bbae51f8d8f477392e8dba9b22c279722446967e
DanielM7911 <dm2775270@gmail.com>
2025-05-20 10:32:08 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     // Llamada a la API con la frase reconocida
-    fetch("http://localhost/2api-gpt-php/endpoints/chat.php", {
+    fetch("http://18.209.213.137/2api-gpt-php/endpoints/chat.php", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ message: transcript }) // CAMBIO AQUÍ para que coincida con el PHP
